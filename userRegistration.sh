006dd0525397368cba0d53977218eb16e3fe71a1
Tushar-W <twankhede95@gmail.com>
2020-04-12 10:30:47 +0530

    ADDED:validation of password for min 8 characters

--- userRegistration.sh
+++ userRegistration.sh
@@ -6,6 +6,7 @@ echo "Welcome To User Registration Problem"
 NAME_PATTERN="^[A-Z]{1}[a-z]{3,}$"
 EMAIL_PATTERN="^[0-9a-zA-Z]{1,}([._+-]{1}[a-zA-Z]+)?[@]{1}[0-9a-zA-Z]{1,}[.]{1}[a-z]{2,4}([.]{1}[a-z]{2})?$"
 MOBILE_NO_PATTERN="^[0-9]{1,3}[ ][0-9]{10}$"
+PASSWORD_PATTERN="^[a-zA-z]{8,}$"
 
 #check pattern is valid or not
 function checkPattern() {
@@ -25,3 +26,5 @@ read -p "Enter Email Address:" email
 checkPattern $email $EMAIL_PATTERN
 read -p "Enter Mobile Number:" mNumber
 checkPattern "$mNumber" "$MOBILE_NO_PATTERN"
+read -p "Enter Password:" password
+checkPattern $password $PASSWORD_PATTERN
